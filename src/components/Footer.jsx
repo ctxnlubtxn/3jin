2522b5be60990a2d861f73f4303a73752d060343
ctxnlubtxn <101271150+ctxnlubtxn@users.noreply.github.com>
2023-09-08 23:09:13 +0700

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer>
-      <div className="flex justify-center items-center h-20 bg-white text-slate-900 border-t border-slate-300">
 
-      </div>
     </footer>
   );
 };
