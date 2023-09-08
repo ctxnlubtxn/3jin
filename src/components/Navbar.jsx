2367d969b5b67c939823f019c97a4d6509f9b5d5
ctxnlubtxn <101271150+ctxnlubtxn@users.noreply.github.com>
2023-09-08 23:11:11 +0700

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { AiFillGithub } from "react-icons/ai";
 const Navbar = () => {
   return (
-    <nav className="w-full flex justify-between items-center border-b border-slaye-400 py-4 px-3 lg:px-20">
+    <nav>
 
     </nav>
   );
