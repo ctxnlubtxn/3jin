69d9e3e8935a585c3a6e905f5154d870dbf0e9ab
ctxnlubtxn <101271150+ctxnlubtxn@users.noreply.github.com>
2023-09-08 23:08:23 +0700

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,16 +4,7 @@ const Footer = () => {
   return (
     <footer>
       <div className="flex justify-center items-center h-20 bg-white text-slate-900 border-t border-slate-300">
-        <p className="text-center">
-          made with ❤️ by{" "}
-          <a
-            href="https://www.iniopik.my.id/"
-            target="_blank"
-            className="font-semibold text-blue-500"
-          >
-            Taufik Hidayatulloh
-          </a>
-        </p>
+
       </div>
     </footer>
   );
